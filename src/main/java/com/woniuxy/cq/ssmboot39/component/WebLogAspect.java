6b48baba4be16b8ce228d3a4a57ebba08da1059e
Lucas <stick.goal@163.com>
2020-09-08 11:48:22 +0800

    修改controller层日志打印，增加执行时间及来源ip，优化格式

--- src/main/java/com/woniuxy/cq/ssmboot39/component/WebLogAspect.java
+++ src/main/java/com/woniuxy/cq/ssmboot39/component/WebLogAspect.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -23,47 +24,85 @@ import java.util.Map;
 public class WebLogAspect {
 
     @Pointcut("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
-    public void reqPointcut(){};
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
-    public void postPointcut(){};
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
-    public void getPointcut(){};
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
-    public void deletePointcut(){};
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.PutMapping)")
-    public void putPointcut(){};
-
-    @Pointcut("reqPointcut() || postPointcut() || getPointcut() || deletePointcut() || putPointcut()")
-    public void logPointcut(){};
-
-    @Before("logPointcut()")
-    public void log(JoinPoint joinPoint){
-
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
-
-        log.debug(">>URL : " + request.getRequestURL().toString());
-        log.debug(">>HTTP_METHOD : " + request.getMethod());
-        log.debug(">>IP : " + request.getRemoteAddr());
-        log.debug(">>CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        if(parameterMap!=null&&parameterMap.size()>0) {
-            Map<String, String> printMap = new HashMap<>();
-
-            parameterMap.forEach((k, v) -> {
-                printMap.put(k, v.length == 1 ? v[0] : Arrays.toString(v));
-            });
-            log.debug(">>ARGS : " + (printMap));
-        }else {
-            log.debug(">>ARGS : []");
-        }
-
-
+    public void reqPointcut() {
     }
 
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+    public void postPointcut() {
+    }
 
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void getPointcut() {
+    }
 
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+    public void deletePointcut() {
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PutMapping)")
+    public void putPointcut() {
+    }
+
+    @Pointcut("reqPointcut() || postPointcut() || getPointcut() || deletePointcut() || putPointcut()")
+    public void logPointcut() {
+    }
+
+    @Around("logPointcut()")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        //获取
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        //启动计时器
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        //执行请求
+        Object result = joinPoint.proceed();
+        //停止计时器
+        stopWatch.stop();
+        //打印
+        log.debug("--==>请求{} [{}]-处理方法{}-参数为：{}-执行时间 {} ms 请求来源IP：{}",
+                request.getRequestURI(),
+                request.getMethod(),
+                joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName(),
+                //从请求中获取参数，不能处理@RequestBody的情况，使用controller的java方法参数可以
+                // 但是参数绑定异常时无法处理，考虑到主要是为了记录正常请求，选择方法参数
+                Arrays.toString(joinPoint.getArgs()),
+                stopWatch.getTotalTimeMillis(),
+                getIPAddress(request));
+
+        return result;
+    }
+
+    public static String getIPAddress(HttpServletRequest request) {
+        String ip = null;
+        //X-Forwarded-For：Squid 服务代理
+        String ipAddresses = request.getHeader("X-Forwarded-For");
+        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            //Proxy-Client-IP：apache 服务代理
+            ipAddresses = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            //WL-Proxy-Client-IP：weblogic 服务代理
+            ipAddresses = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            //HTTP_CLIENT_IP：有些代理服务器
+            ipAddresses = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            //X-Real-IP：nginx服务代理
+            ipAddresses = request.getHeader("X-Real-IP");
+        }
+        //有些网络通过多层代理，那么获取到的ip就会有多个，一般都是通过逗号（,）分割开来，并且第一个ip为客户端的真实IP
+        if (ipAddresses != null && ipAddresses.length() != 0) {
+            ip = ipAddresses.split(",")[0];
+        }
+        //还是不能获取到，最后再通过request.getRemoteAddr();获取
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
 
 
 }
